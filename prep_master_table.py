136aaf93dafccd307f8b44595e0f044c6811d503
ywy1217 <ywy1217@gmail.com>
2021-03-07 10:21:49 -0800

    update

---
+++ prep_master_table.py
@@ -0,0 +1,4 @@
+import pandas
+
+raw_data = pandas.read_csv('master_table_raw_dry_run.csv')
+print(type(raw_data))
--- scrape_master_table.js
+++ scrape_master_table.js
@@ -6,17 +6,19 @@ var stringify = require('csv-stringify');
 
 var URL = 'https://egov.uscis.gov/casestatus/mycasestatus.do?appReceiptNum=RECEIPT_NUM';
 var PREFIX = 'MSC';
-var START_NUMBER = 2190232908;
-var END_NUMBER = 2190232910;
-// var END_NUMBER = 2190442006;
-var saveFile = 'test_data.csv'
+var START_NUMBER = 2190301567;
+var END_NUMBER = 2190442006;
+var saveFile = 'master_table_raw.csv';
+var totalRetrieved = 0;
 
 var receiptNumbers = [];
 for (var i = START_NUMBER; i < END_NUMBER; i++) {
   receiptNumbers.push(PREFIX + i);
 }
 fs.writeFileSync(saveFile, 'receiptID,title,text\n');
-console.log('Start crawling: '+receiptNumbers.length);
+var grandTotal = receiptNumbers.length;
+console.log('Start crawling: '+grandTotal);
+const startTime = Date.now();
 
 function retrieveReceiptNumber(receiptNumber, callback) {
   request({
@@ -33,16 +35,21 @@ function retrieveReceiptNumber(receiptNumber, callback) {
       var $ = cheerio.load(body);
       var title = $('.appointment-sec').find('.text-center').find('h1').text();
       var description = $('.appointment-sec').find('.text-center').find('p').text();
-	  var form_num = description.match(/Form \w-\d\d\d/);
+      var form_num = description.match(/Form \w-\d\d\d/);
       var violation = $('label[for=accessviolation]').text();
+      setTimeout(() => {  console.log("Wait 0.5s!"); }, 500);
 
       if (title.length == 0) {
         if (violation.length > 0) {
           console.log('access violation');
         }
-
         callback();
-      } else {
+        
+      } else if (form_num != "Form I-485") {
+          console.log(receiptNumber + ' is ' + form_num + ', bypass');
+		  callback();
+      }
+      else {
         var row = [
           [receiptNumber, title, description]
         ];
@@ -55,17 +62,21 @@ function retrieveReceiptNumber(receiptNumber, callback) {
             if (err) {
               console.error(err);
             }
+            totalRetrieved = totalRetrieved + 1;
             console.log(receiptNumber + ' written');
+            console.log('Retrieved: '+ totalRetrieved +'/'+ grandTotal+'. Time Lapsed ('+Math.floor((Date.now() - startTime) / 60000)+'min)');
             callback();
           });
         });
+
       }
     });
 }
 
-async.eachLimit(receiptNumbers, 50, retrieveReceiptNumber, function (err) {
+async.eachLimit(receiptNumbers, 7, retrieveReceiptNumber, function (err) {
     if (err) {
         console.error(err);
     }
     console.log('DONE');
 });
+
